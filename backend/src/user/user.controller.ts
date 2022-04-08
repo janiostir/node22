de50cb9b56ebe8cd56cd1b8114b72d012593e38b
unknown <Student@108-15.vssdomena.scv.si>
2022-04-08 11:45:37 +0200

    CRUD za userja

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,12 +1,39 @@
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
 import {get} from "http";
+import {UserService} from "./user.service";
+import {User} from "./user.entity";
 
 @Controller('users')
 export class UserController {
+    constructor(private userService: UserService) {
+    }
 
     @Get()
     all(){
-        return 'vss je zakon!';
+        return this.userService.all();
+    }
+
+    @Post('user')
+    create(@Body() data): Promise<User>{
+        return this.userService.create(data);
+    }
+
+    @Get(':id')
+    getUserById(@Param(':id') id: number) : Promise<User> {
+        return this.userService.findOne({id});
+    }
+
+    @Put(':id')
+    async update(
+        @Param('id') id:number,
+        @Body() data
+    ) : Promise<User> {
+        return await this.userService.update(id, data);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id:number) : Promise<any> {
+        return this.userService.delete(id);
     }
 
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import {Delete, Injectable} from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {User} from "./user.entity";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User) private readonly userRepository: Repository<User>
+    ) {
+    }
+
+    async all(): Promise<any> {
+        return this.userRepository.find();
+    }
+
+    create(data): Promise<User> {
+        return this.userRepository.save(data);
+    }
+
+    findOne(condition): Promise<User> {
+        return this.userRepository.findOne(condition);
+    }
+
+    async update(id, data): Promise<User> {
+        await this.userRepository.update(id, data);
+        return this.findOne({id});
+    }
+
+    delete(id: number): Promise<any> {
+        return this.userRepository.delete(id);
+    }
+
+
+}
